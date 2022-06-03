696f12f26bf71ca50475d72befef637f2ba829d2
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-03 17:49:27 +0300

    Style: update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,10 @@
 import { program } from 'commander';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .arguments('<filepath1> <filepath2>')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format');
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
