18d8eb65e4b00a89ad4b417b3be5d58f8451621b
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-03 15:42:33 +0300

    Feat: add new option and args to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
