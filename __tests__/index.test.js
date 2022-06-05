f7ffa82df7f8d3da0ab05b7c1db2f5641439f2e5
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-05 16:40:20 +0300

    Test: update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
-import path from 'path';
+import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
 test('genDiff', () => {
-  const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
-  const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(readFile('__fixtures__/expected_file.txt'));
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(readFile('expected_file.txt'));
 });
