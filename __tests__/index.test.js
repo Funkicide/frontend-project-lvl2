ef6e7383ac5eaaf902006c5c05137e3c4825dd43
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-07 14:31:38 +0300

    Test: add fixtures and test for .yaml comparison

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,20 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-test('genDiff', () => {
+test('genDiff with shallow .json files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toMatch(readFile('expected_file.txt'));
+});
+
+test('genDiff with shallow .yaml files', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toMatch(readFile('expected_file.txt'));
 });
