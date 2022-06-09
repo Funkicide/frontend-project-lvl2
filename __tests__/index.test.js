ee37c8d36dd589d3e374165af6f57d47c0ea1895
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-09 14:45:51 +0300

    Test: add fixtures and tests for nested files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-test('genDiff with shallow .json files', () => {
+test('genDiff with nested .json files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
@@ -12,7 +12,7 @@ test('genDiff with shallow .json files', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toMatch(readFile('expected_file.txt'));
 });
 
-test('genDiff with shallow .yaml files', () => {
+test('genDiff with nested .yaml files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
