fedc0e3f786afc01b7d7733bbf03a6901a6f4320
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-13 16:35:02 +0300

    Feat: add diff gen for nested files

--- src/index.js
+++ src/index.js
@@ -21,30 +21,57 @@ const genDiff = (filepath1, filepath2) => {
   const parsedData1 = parseData(data1, fileExt1);
   const parsedData2 = parseData(data2, fileExt2);
 
-  const keys1 = Object.keys(parsedData1);
-  const keys2 = Object.keys(parsedData2);
-
-  const keys = _.union(keys1, keys2);
-  const sortedKeys = _.sortBy(keys);
-
-  const diff = sortedKeys.reduce((acc, key) => {
-    let str = acc;
-    if (!Object.hasOwn(parsedData1, key)) {
-      str += `  + ${key}: ${parsedData2[key]}\n`;
-    } else if (!Object.hasOwn(parsedData2, key)) {
-      str += `  - ${key}: ${parsedData1[key]}\n`;
-    } else if (parsedData1[key] !== parsedData2[key]) {
-      str += `  - ${key}: ${parsedData1[key]}\n`;
-      str += `  + ${key}: ${parsedData2[key]}\n`;
-    } else {
-      str += `    ${key}: ${parsedData1[key]}\n`;
+  const iter = (currentData1, currentData2) => {
+    if (!_.isObject(currentData1)) {
+      return String(currentData1);
     }
+    if (!_.isObject(currentData2)) {
+      return String(currentData2);
+    }
+    const keys1 = Object.keys(currentData1);
+    const keys2 = Object.keys(currentData2);
 
-    return str;
-  }, '');
-  const result = `{\n${diff}}`;
+    const keys = _.union(keys1, keys2);
+    const sortedKeys = _.sortBy(keys);
 
-  return result;
+    const diff = sortedKeys
+      .map((key) => {
+        const currentValue1 = currentData1[key];
+        const currentValue2 = currentData2[key];
+
+        if (!Object.hasOwn(currentData1, key)) {
+          return { name: key, status: 'added', value: currentValue2 };
+        }
+        if (!Object.hasOwn(currentData2, key)) {
+          return { name: key, status: 'deleted', value: currentValue1 };
+        }
+        if (currentValue1 === currentValue2) {
+          return { name: key, status: 'unchanged', value: currentValue2 };
+        }
+        if (_.isObject(currentValue1) && !_.isObject(currentValue2)) {
+          return {
+            name: key, status: 'changed', oldValue: currentValue1, newValue: currentValue2,
+          };
+        }
+        if (!_.isObject(currentValue1) && _.isObject(currentValue2)) {
+          return {
+            name: key, status: 'changed', oldValue: currentValue1, newValue: currentValue2,
+          };
+        }
+        if (!_.isObject(currentValue1) && !_.isObject(currentValue2)) {
+          return {
+            name: key, status: 'changed', oldValue: currentValue1, newValue: currentValue2,
+          };
+        }
+        if (_.isObject(currentValue1) && _.isObject(currentValue2)) {
+          return { name: key, type: 'nested', value: iter(currentValue1, currentValue2) };
+        }
+      });
+
+    return diff;
+  };
+
+  return iter(parsedData1, parsedData2);
 };
 
 export default genDiff;
