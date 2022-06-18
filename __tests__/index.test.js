1eeee4ce896317b6040711969dc08595969cda28
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-18 16:44:21 +0300

    Test: add test for gendiff with plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+import formatPlain from '../src/formatters/plain.js';
 
 test('stylish genDiff with nested .json files', () => {
   const __filename = fileURLToPath(import.meta.url);
@@ -19,4 +20,13 @@ test('stylish genDiff with nested .yaml files', () => {
   const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toMatch(readFile('expected_file.txt'));
+});
+
+test('plain genDiff with nested files', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), formatPlain)).toMatch(readFile('expected_file_plain.txt'));
 });
