43dfeb51868d48e1ca64b23046b170625dc40f63
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-19 11:56:43 +0300

    Test: add tests for json output format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,31 +2,54 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import formatPlain from '../src/formatters/plain.js';
 
-test('stylish genDiff with nested .json files', () => {
+test('stylish output .json files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toMatch(readFile('expected_file.txt'));
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toMatch(readFile('expected_file.txt'));
 });
 
-test('stylish genDiff with nested .yaml files', () => {
+test('stylish output .yaml files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toMatch(readFile('expected_file.txt'));
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toMatch(readFile('expected_file.txt'));
 });
 
-test('plain genDiff with nested files', () => {
+test('plain output .json files', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), formatPlain)).toMatch(readFile('expected_file_plain.txt'));
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toMatch(readFile('expected_file_plain.txt'));
+});
+test('plain output .yaml files', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toMatch(readFile('expected_file_plain.txt'));
+});
+test('json output .json files', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toMatch(readFile('expected_file_json.txt'));
+});
+test('json output .yaml files', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toMatch(readFile('expected_file_json.txt'));
 });
